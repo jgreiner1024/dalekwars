af99ecf717ebd5d81c2ae74678501b2ac74d3c92
Jeremy Greiner <jgreiner@1024studios.com>
2020-04-09 11:40:46 -0500

    Updated Menu Scene, added controller and button textures.

---
+++ DalekWarsGame/Assets/Scripts/Menu/MainUIController.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainUIController : MonoBehaviour
+{
+    public void PlayButton_OnClick() { }
+    public void AIPlayButton_OnClick() { }
+    public void CreditsButton_OnClick() { }
+    public void QuitButton_OnClick() { }
+
+}
